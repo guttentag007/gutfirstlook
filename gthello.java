485ea26fada77ff2e4288e750d1c705cb0e25083
guttentag007 <guttentag007@gmail.com>
2015-11-04 20:00:42 +0200

    Create first java Hello

---
+++ gthello.java
@@ -0,0 +1,8 @@
+public class gthello
+{
+	public static void main(String[] arg)
+	{
+		System.out.println("Hello World!!!"); // Моя первая строка
+		System.out.println("This was my first look to Java technologie!"); 
+	}
+}
